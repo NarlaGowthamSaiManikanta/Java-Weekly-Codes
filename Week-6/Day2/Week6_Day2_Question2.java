c4a5f73cca2919390e70e71ffc12d28ec6d412f7
Narla Gowtham Sai Manikanta <94338734+NarlaGowthamSaiManikanta@users.noreply.github.com>
2023-03-07 21:41:29 +0530

    Update Week6_Day2_Question2.java

--- Week-6/Day2/Week6_Day2_Question2.java
+++ Week-6/Day2/Week6_Day2_Question2.java
@@ -1,29 +1,3 @@
-/*
-Puneet and Virat are playing a game. Virat tells a number that Puneet need to check whether that number is even and multiple of 3 or not. Write a program in which implement a method public boolean check(int n) which will return true if number satisfy the conditions else return false.
-
-Input Format
-One integer value representing number given by Virat.
-
-Constraints
-Number will lie between 20 and 400.
-
-Output Format
-True/False according to the value returned by the method or will print Invalid Input in case of number did not match the constraints.
-
-Sample Input 0
-60
-
-Sample Output 0
-True
-
-Sample Input 1
-12
-
-Sample Output 1
-Invalid Input
-*/
-
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
