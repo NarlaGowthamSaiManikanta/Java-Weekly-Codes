caa4dbcb8ef9725401ea3ede4ea0a44a776e1756
Narla Gowtham Sai Manikanta <94338734+NarlaGowthamSaiManikanta@users.noreply.github.com>
2023-03-23 00:33:21 +0530

    Create Week2_Day1_Question2.java

---
+++ Week-2/Day1/Week2_Day1_Question2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner obj= new Scanner(System.in);
+        int N=obj.nextInt();
+        int X=obj.nextInt();
+        System.out.println(N+X);
+    }
+}
