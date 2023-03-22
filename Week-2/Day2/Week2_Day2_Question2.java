d296f39c1209cb3771b00a846219bb6224d6b7dc
Narla Gowtham Sai Manikanta <94338734+NarlaGowthamSaiManikanta@users.noreply.github.com>
2023-03-23 00:36:39 +0530

    Create Week2_Day2_Question2.java

---
+++ Week-2/Day2/Week2_Day2_Question2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner obj=new Scanner(System.in);
+        float n=obj.nextFloat();
+        if(n>=1f&&n<=5f)
+            System.out.println("Thanks");
+        else
+            System.out.println("Error: 101");
+    }
+}
